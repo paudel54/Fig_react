16085c4e656e53275693e3c7c6422c655637f2d6
paudel54 <2sansrit@gmail.com>
2023-09-17 17:19:04 +0545

    bug fixed on start page with responsiveness

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -12,15 +12,17 @@ const Start = () => {
           data-aos-delay="400"
           data-aos-duration="2000"
         >
-          <h1 className="text-[#FFFFFF] lg:mb-4 opacity-50">START WITH US</h1>
-          <h1 className="text-[2rem] lg:text-[3rem] font-medium text-white mb-4">
+          <h1 className="ml-2 md:ml-0  text-[#FFFFFF] lg:mb-4 opacity-50">
+            START WITH US
+          </h1>
+          <h1 className="ml-2 md:ml-0 text-[2rem] lg:text-[3rem] font-medium text-white mb-4">
             Start Working <br className="hidden lg:inline" /> Faster Today
           </h1>
-          <p className="text-white mb-6 lg:mb-8 text-sm md:text-base lg:text-lg">
+          <p className="ml-2 md:ml-0  text-white mb-6 lg:mb-8 text-sm md:text-base lg:text-lg">
             We know the best solution for all of your <br /> business ideas and
             we can help you solve <br /> all business problems.
           </p>
-          <Button text="GET STARTED" />
+          <Button text="GET STARTED" className=" ml-2 md:ml-0" />
         </div>
         {/* Requires a fix of z-index  */}
         <div
